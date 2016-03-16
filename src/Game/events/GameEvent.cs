f2a06cb1d3f063e4e3d0b11e840d4d1b2b37f558
amulware <amul@amulware.net>
2016-03-16 19:29:39 +0100

    added base GameEvent class

---
+++ src/Game/events/GameEvent.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using Bearded.Utilities.SpaceTime;
+
+namespace Centipede.Game
+{
+    class GameEvent : IGameEvent
+    {
+        private bool sent;
+
+        public GameEvent(Position2 position, Unit visibleRadius, Unit audibleRadius)
+        {
+            this.Position = position;
+            this.VisibleRadius = visibleRadius;
+            this.AudibleRadius = audibleRadius;
+            this.MaxRadius = visibleRadius > audibleRadius ? visibleRadius : audibleRadius;
+        }
+
+        public Position2 Position { get; }
+        public Unit VisibleRadius { get; }
+        public Unit AudibleRadius { get; }
+        public Unit MaxRadius { get; }
+
+        public void Send(GameState game)
+        {
+            this.ensureOnlySendOnce();
+
+            foreach (var listener in game.Level
+                .TilesIntersecting(this.Position, this.MaxRadius)
+                .Where(tile => tile.IsValid)
+                .SelectMany(tile => tile.Value.EventListeners))
+            {
+                listener.TryPerceive(this);
+            }
+        }
+
+        private void ensureOnlySendOnce()
+        {
+            if (this.sent)
+                throw new Exception("Can only send game event once.");
+
+            this.sent = true;
+        }
+    }
+}
--- src/Game/interfaces/IGameEvent.cs
+++ src/Game/interfaces/IGameEvent.cs
@@ -1,7 +1,14 @@
-﻿namespace Centipede.Game
+﻿using Bearded.Utilities.SpaceTime;
+
+namespace Centipede.Game
 {
     interface IGameEvent
     {
+        Position2 Position { get; }
+        Unit VisibleRadius { get; }
+        Unit AudibleRadius { get; }
+        Unit MaxRadius { get; }
 
+        void Send(GameState game);
     }
 }
